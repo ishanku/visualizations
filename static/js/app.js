8f96fcbbd3ccdedd5affbb42dc262d9cb6be8048
SSiva Raja <ishanku@gmail.com>
2020-03-25 21:03:16 -0400

    sort check

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,8 @@ d3.json("./static/data/samples.json").then((data) => {
 samples=data.samples;
 console.log(samples);
 });
+
+
+
+var sortedBySampleValues = samples.sort((a, b) => b.sample_values - a.sample_values);
+console.log(sortedBySampleValues)
