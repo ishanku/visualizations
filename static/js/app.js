c0fe3a1171d251e2e26dcf55808b4277c05ef14d
SSiva Raja <ishanku@gmail.com>
2020-03-25 21:08:39 -0400

    Top10Slice

--- static/js/app.js
+++ static/js/app.js
@@ -7,3 +7,20 @@ console.log(samples);
 
 var sortedBySampleValues = samples.sort((a, b) => b.sample_values - a.sample_values);
 console.log(sortedBySampleValues)
+
+top10Samples=sortedBySampleValues.slice(0,10);
+
+console.log(top10Samples);
+
+// Reverse the array to accommodate Plotly's defaults
+reversedTop = top10Samples.reverse();
+
+// Trace1 for the Greek Data
+var trace1 = {
+  x: reversedTop.map(object => object.otu_ids),
+  y: reversedTop.map(object => object.sample_values),
+  text: reversedTop.map(object => object.otu_labels),
+  name: "Top 10 OTUs found in individual",
+  type: "bar",
+  orientation: "h"
+};
