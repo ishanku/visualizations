7efc72343ae4f3bfc44c28107948c2db35ace6ec
SSiva Raja <ishanku@gmail.com>
2020-03-31 15:35:19 -0400

    bonus

--- static/js/app.js
+++ static/js/app.js
@@ -69,7 +69,7 @@ function chartNow(xaxis,yaxis,ticks,type,title,orientation,color,colorscale,size
       value: val,
       type: "indicator",
       mode: mode,
-      title: { text: level, font: { size: 24 } },
+      title: { text: level, font: { size: 14 },font : { color: "Purple" } },
       delta: { reference: maxrange/4, increasing: { color: "Purple" } },
       gauge: {
         axis: { range: [null, maxrange], tickwidth: 1, tickcolor: "darkblue" },
@@ -92,8 +92,8 @@ function chartNow(xaxis,yaxis,ticks,type,title,orientation,color,colorscale,size
     }
   var layout = { 
     title: title,
-    width: 600, height: 350, margin: { t: 25, r: 25, l: 25, b: 25 } ,
-    paper_bgcolor: "lavender",
+    width: 600, height: 350, margin: { t: 150, r: 25, l: 25, b: 25 } ,
+    paper_bgcolor: "white",
   font: { color: "darkblue", family: "Arial" }
   };
   }
@@ -179,62 +179,6 @@ function table(data){
     }
   });
 
-        var level = wfreq*20;
-        // Trig to calc meter point
-        var degrees = 180 - level,
-            radius = .5;
-        var radians = degrees * Math.PI / 180;
-        var x = radius * Math.cos(radians);
-        var y = radius * Math.sin(radians);
-        // Path: may have to change to create a better triangle
-        var mainPath = 'M -.0 -0.05 L .0 0.05 L ',
-            pathX = String(x),
-            space = ' ',
-            pathY = String(y),
-            pathEnd = ' Z';
-        var path = mainPath.concat(pathX,space,pathY,pathEnd);
-        var data = [{ type: 'scatter',
-        x: [0], y:[0],
-            marker: {size: 12, color:'850000'},
-            showlegend: false,
-            name: 'Freq',
-            text: level,
-            hoverinfo: 'text+name'},
-        { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
-        rotation: 90,
-        text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
-        textinfo: 'text',
-        textposition:'inside',
-        marker: {
-            colors:[
-                'rgba(0, 105, 11, .5)', 'rgba(10, 120, 22, .5)',
-                'rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
-                'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
-                'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
-                'rgba(240, 230, 215, .5)', 'rgba(255, 255, 255, 0)']},
-        labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
-        hoverinfo: 'label',
-        hole: .5,
-        type: 'pie',
-        showlegend: false
-        }];
-        var layout = {
-        shapes:[{
-            type: 'path',
-            path: path,
-            fillcolor: '850000',
-            line: {
-                color: '850000'
-            }
-            }],
-        title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
-        height: 500,
-        width: 500,
-        xaxis: {zeroline:false, showticklabels:false,
-                    showgrid: false, range: [-1, 1]},
-        yaxis: {zeroline:false, showticklabels:false,
-                    showgrid: false, range: [-1, 1]}
-        };
-        Plotly.newPlot("gauge-small", data, layout);
+smallguage(wfreq);
 
 }
